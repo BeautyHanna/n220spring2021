af88c63359c13b607fd213a8014f727622415322
Jamil, Niya <njamil@iu.edu>
2021-01-28 21:04:59 -0500

    draw background

--- labs/lab1/js/app.js
+++ labs/lab1/js/app.js
@@ -8,11 +8,20 @@ var midY = 300
 
 function draw(){
 
+    fill(112, 224, 255)
+    noStroke()
+    rect(0,0,800,250)
     //draw the stick of a flower
     stroke(0)
     strokeWeight(0.5)
     fill(179, 76, 12)
-    rect(380,midY,10,300)
+    rect(380,290,10,310)
+
+    //draw thorns on the stick
+    triangle(379,335,355,345,379,345)
+    triangle(391,400,410,400,391,405)
+    triangle(379,460,355,460,379,470)
+    triangle(391,560,410,575,391,570)
 
     //draw leaves of a flower
     let leafY = 280
@@ -31,5 +40,12 @@ function draw(){
     bezier(425,leafY,420,290,415,310,445,320)
     bezier(425,leafY,470,290,440,310,445,330)
 
-    
+    //draw rose bud
+    fill(186, 6, 0)
+    rect(320,145,125,150,40)
+
+    //draw rose petals
+    ellipse(383, 172,120,60)
+    ellipse(383, 169,90,40)
+    ellipse(383, 166,60,20)
 }
