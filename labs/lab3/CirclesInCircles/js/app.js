5dec3663dae6bd2149fac1ea5e331e3c04d5e5d1
Jamil, Niya <njamil@iu.edu>
2021-02-15 23:46:53 -0500

    comments

--- labs/lab3/CirclesInCircles/js/app.js
+++ labs/lab3/CirclesInCircles/js/app.js
@@ -7,7 +7,9 @@ function setup(){
 function draw(){
 
     noFill();
+    //40 is the number of circles
     for(var i = 1; i < 40; i++){
+        //draw circles at x and y position and diameter increases by 5 for every circle drawn
         circle(200,200, i * 5);
     }
 }
