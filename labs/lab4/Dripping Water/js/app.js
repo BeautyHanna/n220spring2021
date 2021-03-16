4317348c48e015b5b2d53799cb61db1d2fa65907
Jamil, Niya <njamil@iu.edu>
2021-03-16 11:24:29 -0400

    Fix Water

--- labs/lab4/Dripping Water/js/app.js
+++ labs/lab4/Dripping Water/js/app.js
@@ -15,7 +15,7 @@ function draw() {
     
 
     
-    
+    //draw circle and makes it drop at 5px per frame
     for( var i = 0; i < PosY.length; i ++) {
         
         fill(45, 178, 235)
@@ -25,15 +25,19 @@ function draw() {
         
     }
 
-    frameRate = frameRate + 1 //PosY = PosY + SpeedY;
+    //to increase the frame 
+    frameRate = frameRate + 0.5
 
+    //every 10 frame draw a circle at the top of the canvas
    if (frameRate == 10){
         frameRate = 0
 
+        //add to the end of the array 
         PosY.push(0)
 
         if (PosY.length > 50){
-
+            
+            //remove a position Y from the beginning of the array 
             PosY.length.shift();
         }
     } 
