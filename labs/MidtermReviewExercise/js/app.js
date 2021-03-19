7c1a0b2ad08e9f4d8c1aecf64cfe48bb62a6e928
Jamil, Niya <njamil@iu.edu>
2021-03-18 23:52:15 -0400

    Ball Bounce

--- labs/MidtermReviewExercise/js/app.js
+++ labs/MidtermReviewExercise/js/app.js
@@ -1,9 +1,33 @@
+let PosX =400;
+let PosY = 300; 
+
+let SpeedX = 3;
+let SpeedY = 3;
+
+
 function setup(){
-    createCanvas(600, 600);
+    createCanvas(800, 600);
 }
 
 function draw(){
     background(50)
 
-    rect(550,mouseY,30,50)
+    rect(770,mouseY,30,50)
+
+    circle(PosX, PosY, 50);
+
+    PosX = PosX + SpeedX;
+    PosY = PosY + SpeedY;
+    
+    
+    if (PosY > 600){
+        SpeedY = -3;
+    } else if (PosX > 800){
+        SpeedX = -3;
+    } else if (PosY < 0){
+        SpeedY = 3;
+    } else if (PosX < 0){
+        SpeedX = 3;
+    }
+  
 }
