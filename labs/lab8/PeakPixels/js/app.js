cbe5b6e988ed2de8d325edc38bcb66836d4bdb3e
Jamil, Niya <njamil@iu.edu>
2021-04-09 01:58:31 -0400

    PeakPixels

---
+++ labs/lab8/PeakPixels/js/app.js
@@ -0,0 +1,20 @@
+let GreenSquare = document.getElementById ("GreenSquare");
+var side = 100
+
+
+
+GreenSquare.style.backgroundColor = "#7bc990";
+GreenSquare.style.width = side + "px";
+GreenSquare.style.height = side + "px";
+
+function IncreaseSize() {
+       
+    
+    side = side * 1.1;
+    
+    GreenSquare.style.height = side + "px";
+    GreenSquare.style.width = side + "px";
+
+
+}
+
