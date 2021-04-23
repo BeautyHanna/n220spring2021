c1575b88d24c7556d28c2301103f3525376fbb59
Jamil, Niya <njamil@iu.edu>
2021-04-22 23:49:44 -0400

    Folders Created

---
+++ Practice/Week10/js/app.js
@@ -0,0 +1,15 @@
+let books = ["Harry Potter", "Hunger Games", "Lord of The Rings", "Narnia"]
+
+/*dfunction findBook() {
+  document.getElementById("Title").innerHTML = books.find(function (book){;
+  return book == "Harry Potter";
+})
+}*/
+
+function findBook(){
+    document.getElementById("Title").innerHTML = books.find(function (book){
+
+        return book == ["];
+    })
+}
+
