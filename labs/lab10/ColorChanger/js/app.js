d36e1c4b3d08339b5540b4090949e6bba2da8169
BeautyHanna <65430214+BeautyHanna@users.noreply.github.com>
2021-04-23 11:38:59 -0400

    colorGrey

---
+++ labs/lab10/ColorChanger/js/app.js
@@ -0,0 +1,7 @@
+let GreySquares = document.getElementById ("GreySquares");
+
+var side = 200
+GreySquares.style.width = side + "px";
+GreySquares.style.height = side + "px";
+GreySquares.style.backgroundColor = "#8f8c85";
+GreySquares.style.float = left;
