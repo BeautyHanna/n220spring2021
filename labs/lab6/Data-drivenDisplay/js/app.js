79625beede82331f3d612732d5f07e0c71ace975
Jamil, Niya <njamil@iu.edu>
2021-05-04 11:42:55 -0400

    Objects

--- labs/lab6/Data-drivenDisplay/js/app.js
+++ labs/lab6/Data-drivenDisplay/js/app.js
@@ -1,6 +1,37 @@
 let myPhone = {
-    make: "Apple",
-    model: "iPhone11",
-    color: "gold",
-    size: "ProMax",
+    color: "#fbd7bd",
+    height: 600,
+    width: 300,
+    xPos: 200,
+    yPos: 200,
+    phoneRadius: 10
+
+}
+
+let iPhoneCamera = {
+    color: "#000000" ,
+    xPos: 210,
+    yPos: 210,
+    radius: 20,
+
+}
+
+
+function setup(){
+
+    createCanvas(800, 800)
+
+    iPhone()
+}
+
+function iPhone (){
+    fill (myPhone.color)
+    rect(myPhone.xPos, myPhone.yPos, myPhone.width, myPhone.height, myPhone.phoneRadius )
+    
+
+}
+
+function iPhoneCamera(){
+
+
 }
