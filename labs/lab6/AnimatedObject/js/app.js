065f8f4b817c9990347c9bfd28ce6d490345361d
Jamil, Niya <njamil@iu.edu>
2021-05-04 13:12:10 -0400

    Animated Object

--- labs/lab6/AnimatedObject/js/app.js
+++ labs/lab6/AnimatedObject/js/app.js
@@ -1,12 +1,28 @@
 myCircle = {
     X: 200,
     Y: 200,
-    R: 50
+    R: 50,
+    C: "#fa64aa",
+    S: 1,
 }
 
 function setup(){
 
     createCanvas(800, 800)
-
     
 }
+
+function draw () {
+    
+    fill(myCircle.C);
+    circle(myCircle.X, myCircle.Y, myCircle.R)
+    myCircle.Y = myCircle.Y + myCircle.S;
+    
+    if (myCircle.Y == 400){
+        myCircle.S = -1;
+    }else if (myCircle.Y == 200){
+        myCircle.S = 1;
+    }
+ 
+}
+
